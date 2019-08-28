56471611652fe10c3f12ea00d23b0bc380f47a35
juankbrera <jckbrera@gmail.com>
2019-08-28 17:04:34 -0600

    Products resource created

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'sku' => $this->sku,
+            'slug' => $this->slug,
+            'name' => $this->name,
+            'photo' => env('APP_URL') .'/storage/uploads/images/products/'. $this->photo,
+            'description' => $this->description,
+            'price' => '$'.number_format((float) $this->price, 2, '.', ','),
+            'stock' => $this->stock,
+            'is_active' => $this->is_active,
+            'created_at' => $this->created_at,
+            'update_at' => $this->update_at,
+        ];
+    }
+}
