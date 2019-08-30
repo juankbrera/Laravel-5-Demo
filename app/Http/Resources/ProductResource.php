c98ddb35852085b449a2af701260300bdaca3093
juankbrera <jckbrera@gmail.com>
2019-08-30 17:35:34 -0600

    ProductResource refactoring

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -15,17 +15,17 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'sku' => $this->sku,
-            'slug' => $this->slug,
-            'name' => $this->name,
-            'photo' => env('APP_URL') .'/storage/uploads/images/products/'. $this->photo,
+            'id'          => $this->id,
+            'sku'         => $this->sku,
+            'slug'        => $this->slug,
+            'name'        => $this->name,
+            'photo'       => env('APP_URL') .'/storage/uploads/images/products/'. $this->photo,
             'description' => $this->description,
-            'price' => number_format((float) $this->price, 2, '.', ','),
-            'stock' => $this->stock,
-            'is_active' => $this->is_active,
-            'created_at' => $this->created_at,
-            'update_at' => $this->update_at
+            'price'       => number_format((float) $this->price, 2, '.', ','),
+            'stock'       => $this->stock,
+            'is_active'   => $this->is_active,
+            'created_at'  => $this->created_at,
+            'update_at'   => $this->update_at
         ];
     }
 }
