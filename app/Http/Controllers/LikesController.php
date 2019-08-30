b5257bd02050c5b4f37e1d2d3146bf214a31c737
juankbrera <jckbrera@gmail.com>
2019-08-30 14:22:35 -0600

    Likes controler created

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -2,10 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ProductLike;
+use App\Models\Like;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class LikesController extends Controller
 {
+    /**
+    * Like model
+    *
+    * @var \App\Models\Like
+    */
+    public $like;
+
+    /**
+    * Product model
+    *
+    * @var \App\Models\Product
+    */
+    public $product;
+
+    /**
+    * Create a new controller instance.
+    *
+    * @param  \App\Models\Like         $likes
+    * @param  \App\Models\Product      $products
+    */
+    public function __construct(
+        Like $like,
+        Product $product
+    ) {
+        $this->like    = $like;
+        $this->product = $product;
+    }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -14,7 +45,17 @@ class LikesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $product      = $this->product->findOrFail($request->product_id);
+        $product_like = $product->likes()->firstOrCreate([
+            'user_id' => auth('api')->user()->id
+        ]);
+
+        if ($product_like->wasRecentlyCreated) {
+            event(new ProductLike($product));
+        }
+
+        return response()->json([
+            'message' => 'Like stored successfully!'], 201);
     }
 
     /**
@@ -23,8 +64,18 @@ class LikesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request, $id)
     {
-        //
+        $product      = $this->product->findOrFail($request->product_id);
+        $product_like = $product->likes()
+            ->where('user_id', auth('api')->user()->id)
+            ->firstOrFail();
+
+        $product_like->delete();
+
+        event(new ProductLike($product, false));
+
+        return response()->json([
+            'message' => 'Like destroyed successfully!'], 201);
     }
 }
