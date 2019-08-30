aa672d7444fa0b6cf3d3d2fce2b4c24dc1fee50d
juankbrera <jckbrera@gmail.com>
2019-08-30 17:40:47 -0600

    OrderController refactoring

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,20 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderStoreRequest;
-use App\Models\Order;
-use App\Models\OrderItem;
+use App\Http\Resources\OrderResource;
 use App\Services\OrderService;
-use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
-    /**
-    * Order model
-    *
-    * @var \App\Models\Order
-    */
-    protected $order;
-
     /**
      * Order service
      *
@@ -27,32 +18,19 @@ class OrdersController extends Controller
     /**
      * Create a new controller instance.
      *
-     * @param  \App\Models\Order    $orders
      * @param  \App\Services\OrderService  $order_service
      */
     public function __construct(
-        Order $order,
         OrderService $order_service
     ) {
-        $this->order   = $order;
         $this->order_service = $order_service;
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  App\Http\Requests\OrderStoreRequest  $request
-     * @return string
+     * @return App\Http\Resources\OrderResource
      */
     public function store(OrderStoreRequest $request)
     {
@@ -60,41 +38,6 @@ class OrdersController extends Controller
 
         $order = $this->order_service->placeOrder($validated_data['items']);
 
-        return response()->json([
-            'message' => 'Order successfully placed!'], 201);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return new OrderResource($order);
     }
 }
