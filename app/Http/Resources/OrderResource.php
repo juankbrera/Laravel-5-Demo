25d29b6ebd76c8d7d0c4a4834a9451a6fc047008
juankbrera <jckbrera@gmail.com>
2019-08-30 17:35:20 -0600

    OrderResource created

---
+++ app/Http/Resources/OrderResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'           => $this->id,
+            'order_number' => $this->order_number,
+            'user_id'      => $this->user_id,
+            'total_amount' => $this->total_amount,
+            'is_placed'    => $this->is_placed,
+        ];
+    }
+}
