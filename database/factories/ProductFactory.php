d175f5c831a85394a9143d7122d72a7f6f06ccf2
juankbrera <jckbrera@gmail.com>
2019-08-31 15:01:03 -0600

    Product seed & factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Models\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'sku'          => $faker->uuid,
+        'slug'         => $faker->unique()->slug,
+        'name'         => $faker->city,
+        'photo'        => $faker->image('public/storage/uploads/images/products', 400, 400, 'food', false),
+        'description'  => $faker->text($maxNbChars = 100),
+        'price'        => $faker->randomFloat(2, 1, 10),
+        'stock'        => $faker->randomNumber(3),
+        'is_active'    => true,
+    ];
+});
